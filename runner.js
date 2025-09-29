5b0a46c58374234e73c2239fff2242864e60578a
Jake Norton <jake.norton88@gmail.com>
2025-09-29 15:39:25 +1300

    core gameplay loop established

--- runner.js
+++ runner.js
@@ -33,6 +33,17 @@ const state = {
     arrows: []
 }
 
+function rect_rect_collision(r1, r2) {
+    if (r1.x + r1.width >= r2.x && // r1 right edge past r2 left
+	r1.x <= r2.x + r2.width && // r1 left edge past r2 right
+	r1.y + r1.height >= r2.y && // r1 top edge past r2 bottom
+	r1.y <= r2.y + r2.height) { // r1 bottom edge past r2 top
+	return true;
+    }
+
+    return false;
+}
+
 function lerp(from, to, weight) {
     return from + (to - from) * weight
 }
@@ -53,32 +64,44 @@ function clear_screen(colour) {
 
 class Player {
     constructor(ctx) {
-	this.colour = "black";
-	this.x = 20;
-	this.y = height / 2;
-	this.velocityY = 0;  
-	this.width = 20;
-	this.height = 40;
+	this.colour = "black"
+	this.x = 20
+	this.y = height / 2
+	this.velocityY = 0  
+	this.width = 20
+	this.height = 40
 	this.extra_hitbox = 3
 	this.ctx = ctx
-	this.jump_height = 100;
-	this.jump_time_to_peak = 0.5;
-	this.jump_time_to_descent = 0.3;
+	this.jump_height = 100
+	this.jump_time_to_peak = 0.5
+	this.jump_time_to_descent = 0.3
+	this.collided = false
 	this.update_jump_values()
     }
 
     update_jump_values() {
-	this.jump_velocity = ((2.0 * this.jump_height) / this.jump_time_to_peak) * -1.0;
-	this.jump_gravity = ((-2.0 * this.jump_height) / (this.jump_time_to_peak * this.jump_time_to_peak)) * -1.0;
-	this.fall_gravity = ((-2.0 * this.jump_height) / (this.jump_time_to_descent * this.jump_time_to_descent)) * -1.0;
+	this.jump_velocity = ((2.0 * this.jump_height) / this.jump_time_to_peak) * -1.0
+	this.jump_gravity = ((-2.0 * this.jump_height) / (this.jump_time_to_peak * this.jump_time_to_peak)) * -1.0
+	this.fall_gravity = ((-2.0 * this.jump_height) / (this.jump_time_to_descent * this.jump_time_to_descent)) * -1.0
     }
 
     update() {
-	this.velocityY += this.get_gravity() * deltaTime;
-
+	this.velocityY += this.get_gravity() * deltaTime
 	this.y += this.velocityY * deltaTime
-
 	if (this.y + this.height > state.floor.y) this.y = state.floor.y - this.height
+	this.check_collision()
+    }
+
+    check_collision() {
+	for (let i = 0; i < spawner.hazards.items.length; ++i) {
+	    const current_hazard = spawner.hazards.items[i]
+	    if (current_hazard.is_alive) {
+		if (rect_rect_collision(this, current_hazard)) {
+		    current_hazard.colour = "red"
+		    this.collided = true
+		}
+	    }
+	}
     }
 
     draw() {
@@ -88,16 +111,16 @@ class Player {
 
     jump() {
 	if (this.is_on_floor()) {
-	    this.velocityY = this.jump_velocity;
+	    this.velocityY = this.jump_velocity
 	}
     }
 
     get_gravity() {
-	return this.velocityY < 0.0 ? this.jump_gravity : this.fall_gravity;
+	return this.velocityY < 0.0 ? this.jump_gravity : this.fall_gravity
     }
 
     is_on_floor() {
-        return this.y + this.height + this.extra_hitbox >= state.floor.y ? true : false;
+        return this.y + this.height + this.extra_hitbox >= state.floor.y ? true : false
     }
 }
 
@@ -160,11 +183,11 @@ class Spawner {
             y: y_offset, 
             width:  hazard_width, 
             height: hazard_height, 
-            colour: "black",
+            colour: "purple",
 	    speed_multiplier: 1,
             is_alive: true
         }
-	console.log("hazard: ", this.hazards.items[index])
+	// console.log("hazard: ", this.hazards.items[index])
         this.hazards.count += 1
     }
     
@@ -186,7 +209,7 @@ class Spawner {
             speed_multiplier: speed_multiplier,
             is_alive: true
         }
-	console.log("arrow: ", this.arrows.items[index])
+	// console.log("arrow: ", this.arrows.items[index])
         this.arrows.count += 1
     }
     
@@ -198,7 +221,7 @@ class Spawner {
 	    // Find first dead arrow and reuse it
 	    for (let i = 0; i < this.arrows.items.length; i++) {
 		if (!this.arrows.items[i].is_alive) {
-		    console.log(`resurrecting arrow:${i}`)
+		    // console.log(`resurrecting arrow:${i}`)
 		    this.spawn_arrow(i)
 		    break;
 		}
@@ -224,7 +247,7 @@ class Spawner {
 	if (this.hazards.count < this.hazards.max && this.hazards.spawn_timer >= this.hazards.spawn_rate) {
 	    for (let i = 0; i < this.hazards.items.length; i++) {
 		if (!this.hazards.items[i].is_alive) {
-		    console.log(`resurrecting hazard: ${i}`)
+		    // console.log(`resurrecting hazard: ${i}`)
 		    this.spawn_hazard(i)
 		    break;
 		}
@@ -299,19 +322,41 @@ function draw_background() {
     spawner.draw()
 }
 
+function draw_text(fillStyle, font, text, x, y) {
+    ctx.fillStyle = fillStyle
+    ctx.font = font
+    const textMetrics = ctx.measureText(text)
+    ctx.fillText(text, x - textMetrics.width / 2, y)
+}
+
 function update() {
-    clear_screen("skyblue")
+    if (player.collided) {
 
-    draw_underfloor()
+	clear_screen("black")
+	state.background_speed = 0
+	ctx.fillStyle = "red";
+	ctx.font = "30px Arial";
 
-    spawner.update()
-    spawner.draw()
+	score += deltaTime * state.background_speed
+	const game_over= `Gameover, you scored: ${Math.round(score)}`
+	draw_text("red", "30px Arial", game_over, width/2, height/2)
 
-    ctx.fillStyle = state.floor.colour
-    ctx.fillRect(state.floor.x, state.floor.y, state.floor.width, state.floor.height)
+	const restart = `Press R to restart`
+	draw_text("red", "20px Arial", restart, 100, 100)
 
-    player.update()
-    player.draw()
+    } else {
+	clear_screen("skyblue")
+	draw_underfloor()
+
+	spawner.update()
+	spawner.draw()
+
+	ctx.fillStyle = state.floor.colour
+	ctx.fillRect(state.floor.x, state.floor.y, state.floor.width, state.floor.height)
+
+	player.update()
+	player.draw()
+    }
 }
 
 const loop = (timestamp) => {
@@ -319,19 +364,30 @@ const loop = (timestamp) => {
     prevTimestamp = timestamp
     update(timestamp)
 
-    // Simple FPS counter
-    const fps = Math.round(1 / deltaTime);
-    ctx.fillStyle = "black";
-    ctx.font = "16px Arial";
-    ctx.fillText(`FPS: ${fps}`, 10, 30);
+    // const fps = Math.round(1 / deltaTime);
+    // ctx.fillStyle = "black";
+    // ctx.font = "16px Arial";
+    // ctx.fillText(`FPS: ${fps}`, 10, 30);
 
     ctx.fillText
     ctx.fillStyle = "red";
     ctx.font = "20px Arial";
 
-    score += deltaTime * state.background_speed
+    if (!player.collided) {
+        score += deltaTime * state.background_speed
+    }
     ctx.fillText(`Score: ${Math.round(score)}`, width - 100, 30);
+    if (score == 100 && !player.collided) {
+	state.background_speed += 1
+    }
     requestAnimationFrame(loop)
+}
+
+function reset() {
+    player = new Player(ctx)
+    spawner = new Spawner()
+    state.background_speed = 1
+    score = 0
 }
 
 
@@ -343,6 +399,8 @@ function main() {
 	    case "ArrowUp": player.jump(); break;
 	    case " ":       player.jump(); break; 
 	    case "w":       player.jump(); break;    
+	    case "r":       reset(); break;
+	    case "R":       reset(); break;
 	}
     });
 
