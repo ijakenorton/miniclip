49ea3d32d75e513416d5b38d3819e7394c4be6a5
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-05 02:32:04 +1300

    Refactor SymbolLane out of SymbolField class

--- games/DDR/ddr.js
+++ games/DDR/ddr.js
@@ -102,42 +102,23 @@ class SymbolField {
         // xRenderOffset is given as a ratio to the canvas width
         this.xRenderOffset = xRenderOffset;
 
-        // The game symbols that need to be hit in time
-        this.gameSymbols = {};
-        this.gameSymbols[DirectionEnum.RIGHT] = [];
-        this.gameSymbols[DirectionEnum.UP] = [];
-        this.gameSymbols[DirectionEnum.LEFT] = [];
-        this.gameSymbols[DirectionEnum.DOWN] = [];
-
-        // The indicators at the bottom of the field that give an indication of timing
-        this.gameSymbolIndicators = {};
-        this.gameSymbolIndicators[DirectionEnum.RIGHT] = new GameSymbol(
+        // Distance until next arrow is spawned
+        this.lanes = {};
+        this.lanes[DirectionEnum.RIGHT] = new SymbolLane(
             DirectionEnum.RIGHT,
-            (this.xRenderOffset + 7 / 8 * SymbolField.fieldWidth),
-            (SymbolField.topMargin + 0.9 * SymbolField.fieldHeight),
-            Colors.white,
-            true
+            this.xRenderOffset + 7 / 8 * SymbolField.fieldWidth
         );
-        this.gameSymbolIndicators[DirectionEnum.UP] = new GameSymbol(
+        this.lanes[DirectionEnum.UP] = new SymbolLane(
             DirectionEnum.UP,
-            (this.xRenderOffset + 5 / 8 * SymbolField.fieldWidth),
-            (SymbolField.topMargin + 0.9 * SymbolField.fieldHeight),
-            Colors.white,
-            true
+            this.xRenderOffset + 5 / 8 * SymbolField.fieldWidth
         );
-        this.gameSymbolIndicators[DirectionEnum.LEFT] = new GameSymbol(
+        this.lanes[DirectionEnum.LEFT] = new SymbolLane(
             DirectionEnum.LEFT,
-            (this.xRenderOffset + 1 / 8 * SymbolField.fieldWidth),
-            (SymbolField.topMargin + 0.9 * SymbolField.fieldHeight),
-            Colors.white,
-            true
+            this.xRenderOffset + 1 / 8 * SymbolField.fieldWidth
         );
-        this.gameSymbolIndicators[DirectionEnum.DOWN] = new GameSymbol(
+        this.lanes[DirectionEnum.DOWN] = new SymbolLane(
             DirectionEnum.DOWN,
-            (this.xRenderOffset + 3 / 8 * SymbolField.fieldWidth),
-            (SymbolField.topMargin + 0.9 * SymbolField.fieldHeight),
-            Colors.white,
-            true
+            this.xRenderOffset + 3 / 8 * SymbolField.fieldWidth
         );
     }
 
@@ -148,11 +129,7 @@ class SymbolField {
             DirectionEnum.LEFT,
             DirectionEnum.DOWN,
         ]) {
-            for (const s of this.gameSymbols[d]) {
-                console.log(s)
-                s.update();
-            }
-            this.gameSymbolIndicators[d].update()
+            this.lanes[d].update();
         }
     }
 
@@ -184,18 +161,54 @@ class SymbolField {
             DirectionEnum.LEFT,
             DirectionEnum.DOWN,
         ]) {
-            for (const s of this.gameSymbols[d]) {
-                s.draw();
-            }
-
-            this.gameSymbolIndicators[d].draw()
+            this.lanes[d].draw();
         }
+    }
+}
+
+class SymbolLane {
+    // The width of the lane, defined in terms of the entire field
+    static laneWidth = SymbolField.fieldWidth / 4;
+
+    constructor(laneSymbolDirection, laneCenterX) {
+        // The symbol direction this lane is supposed to manage
+        this.laneSymbolDirection = laneSymbolDirection
+
+        // xRenderOffset is given as a ratio to the canvas width
+        this.laneCenterX = laneCenterX;
+
+        // The symbols in this lane
+        this.gameSymbols = [];
+
+        // The indicator for this lane
+        this.gameSymbolIndicator = new GameSymbol(
+            this.laneSymbolDirection,
+            this.laneCenterX,
+            SymbolField.indicatorY,
+            Colors.white,
+            true
+        );
+    }
+
+    draw() {
+        for (const s of this.gameSymbols) {
+            s.draw();
+        }
+
+        this.gameSymbolIndicator.draw()
+    }
+
+    update() {
+        for (const s of this.gameSymbols) {
+            s.update();
+        }
+        this.gameSymbolIndicator.update()
     }
 }
 
 class GameSymbol {
 
-    static arrowSize = 0.8 * SymbolField.fieldWidth / 4;
+    static arrowSize = 0.8 * SymbolLane.laneWidth;
     static tailWidth = 0.5 * GameSymbol.arrowSize;
     static headWidth = 1 * GameSymbol.arrowSize;
     static headLength = 0.65; // How long the head is. 0 is no head, 1 is all head
@@ -225,7 +238,7 @@ class GameSymbol {
         [GameSymbol.arrowSize / 2, 0],
     ]
 
-    constructor(direction, x, y, color=Colors.symbolColor, isIndicator=false) {
+    constructor(direction, x, y, color = Colors.symbolColor, isIndicator = false) {
         // Give the symbol direction (what way the arrow points)
         // The X, Y coordinates are given as fractions of the canvas width and height respectively
         // X,Y refer to the *center* of the symbol
@@ -246,7 +259,7 @@ class GameSymbol {
     }
 
     update() {
-        if (!this.isIndicator){
+        if (!this.isIndicator) {
             this.y += gameProps.gameSpeed * gameProps.deltaTime;
         }
 
@@ -355,35 +368,36 @@ class GameManager {
             return;
         }
         let progressDirection = userID === UserEnum.USER_LEFT ? 1 : -1;
-        let symbolField = this.symbolFields[userID];
-        let directionSymbols = symbolField.gameSymbols[direction];
+        let symbolLane = this.symbolFields[userID].lanes[direction];
+        let laneSymbols = symbolLane.gameSymbols;
+        let laneIndicator = symbolLane.gameSymbolIndicator;
 
-        if (directionSymbols.length === 0) {
+        if (laneSymbols.length === 0) {
             // User pressed a button on a lane with nothing in it.
             gameProps.victoryBarProgress += progressDirection * progressRewardMissed;
-            symbolField.gameSymbolIndicators[direction].setHighlightColor(Colors.missedHighlight);
+            laneIndicator.setHighlightColor(Colors.missedHighlight);
             return;
         }
 
         // We know the final GameSymbol is the lowest
-        let lowestSymbolHeight = directionSymbols[directionSymbols.length - 1].y;
+        let lowestSymbolHeight = laneSymbols[laneSymbols.length - 1].y;
         let deltaHeight = Math.abs(SymbolField.indicatorY - lowestSymbolHeight);
         if (deltaHeight > 0.3) {
             // The symbol is simply too high, treat as a miss
             gameProps.victoryBarProgress += progressDirection * progressRewardMissed;
-            symbolField.gameSymbolIndicators[direction].setHighlightColor(Colors.missedHighlight);
+            laneIndicator.setHighlightColor(Colors.missedHighlight);
             return; // Purposefully do not remove symbol
         } else if (deltaHeight > 0.2) {
             gameProps.victoryBarProgress += progressDirection * progressRewardOkay;
-            symbolField.gameSymbolIndicators[direction].setHighlightColor(Colors.okayHighlight);
+            laneIndicator.setHighlightColor(Colors.okayHighlight);
         } else if (deltaHeight > 0.1) {
             gameProps.victoryBarProgress += progressDirection * progressRewardGood;
-            symbolField.gameSymbolIndicators[direction].setHighlightColor(Colors.goodHighlight);
+            laneIndicator.setHighlightColor(Colors.goodHighlight);
         } else {
             gameProps.victoryBarProgress += progressDirection * progressRewardExcellent;
-            symbolField.gameSymbolIndicators[direction].setHighlightColor(Colors.excellentHighlight);
+            laneIndicator.setHighlightColor(Colors.excellentHighlight);
         }
-        symbolField.gameSymbols[d].pop();
+        symbolLane.gameSymbols[d].pop();
     }
 
     update() {
@@ -402,16 +416,17 @@ class GameManager {
                 DirectionEnum.LEFT,
                 DirectionEnum.DOWN,
             ]) {
-                let directionSymbols = symbolField.gameSymbols[d];
+                let symbolLane = symbolField.lanes[d];
+                let directionSymbols = symbolLane.gameSymbols;
                 if (directionSymbols.length === 0) {
                     continue;
                 }
                 // We know the final GameSymbol is the lowest, so only check this one for deletion
                 let directionLowestSymbol = directionSymbols[directionSymbols.length - 1]
                 if (directionLowestSymbol.y - GameSymbol.arrowSize > (SymbolField.fieldHeight + SymbolField.topMargin)) {
-                    symbolField.gameSymbols[d].pop();
+                    symbolLane.gameSymbols.pop();
                     gameProps.victoryBarProgress += progressDirection * progressRewardMissed;
-                    symbolField.gameSymbolIndicators[d].setHighlightColor(Colors.missedHighlight);
+                    symbolLane.gameSymbolIndicator.setHighlightColor(Colors.missedHighlight);
                 }
             }
         }
